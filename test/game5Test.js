5f46eadefe7a379e9e29ce6c23c2894206729d56
atdan <atumadann@gmail.com>
2024-02-07 09:21:13 +0100

    update games 1 to 5 test cases

--- test/game5Test.js
+++ test/game5Test.js
@@ -13,7 +13,18 @@ describe('Game5', function () {
 
     // good luck
 
-    await game.win();
+    let wallet = ethers.Wallet.createRandom();
+    while(wallet.address.slice(0,4) !== "0x00") {
+      wallet = ethers.Wallet.createRandom();
+    }
+
+    const signer = ethers.provider.getSigner(0);
+    await signer.sendTransaction({
+      to: wallet.address,
+      value: ethers.utils.parseEther("1")
+    });
+
+    await game.connect(wallet.connect(ethers.provider)).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
